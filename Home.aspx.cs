62847f10a65085813364aa126f307c9971e328b3
Lorenzo <lorenzo.zuccante@outlook.it>
2024-02-12 15:01:07 +0100

    Aggiungere i file di progetto.

---
+++ Home.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace esercizio1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Buttonoss_Click(object sender, EventArgs e)
+        {
+            string nome = WebConfigurationManager.AppSettings["name"];
+            string cognome = WebConfigurationManager.AppSettings["surname"];
+
+            Labeliss.Text = nome + " " +cognome;
+        }
+    }
+}
